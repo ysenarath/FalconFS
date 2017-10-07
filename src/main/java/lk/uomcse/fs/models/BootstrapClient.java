7e328d167600e20a1c76319737103f27b3cfbfd9
Yasas <wayasas@gmail.com>
2017-10-07 20:33:34 +0530

    creating basic bootstrapserver request and reply. TODO: handle errors

--- src/main/java/lk/uomcse/fs/models/BootstrapClient.java
+++ src/main/java/lk/uomcse/fs/models/BootstrapClient.java
@@ -1,26 +1,65 @@
 package lk.uomcse.fs.models;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import lk.uomcse.fs.messages.RegisterRequest;
+import lk.uomcse.fs.messages.RegisterResponse;
+import lk.uomcse.fs.messages.UnregisterRequest;
+import lk.uomcse.fs.messages.UnregisterResponse;
+import lk.uomcse.fs.udp.UDPClient;
+import lk.uomcse.fs.utils.RequestFailedException;
 
-import java.util.Set;
+import java.util.List;
+import java.util.logging.Logger;
 
-public class BootstrapClient {
-    private String ip;
+public class BootstrapClient extends UDPClient {
+    private final static Logger LOGGER = Logger.getLogger(BootstrapClient.class.getName());
 
-    private int port;
-
-    private Node node;
-
-    public BootstrapClient(String ip, int port) {
-        this.ip = ip;
-        this.port = port;
+    public BootstrapClient(String host, int port) {
+        super(host, port);
     }
 
-    public Set<Node> register(Node me, String name) {
-        throw new NotImplementedException();
+    /**
+     * Registers the node bootstrap from server
+     *
+     * @param name name of the client
+     * @param me   node represented by the name
+     * @return List of nodes if the request is successful
+     */
+    public List<Node> register(String name, Node me) {
+        super.open();
+        RegisterRequest msg = new RegisterRequest(name, me);
+        LOGGER.info(String.format("Requesting Bootstrap Server: %s", msg.toString()));
+        String reply = request(msg.toString()); // Method will wait for reply
+        LOGGER.info(String.format("Bootstrap Server replied: %s", reply));
+        RegisterResponse rsp = RegisterResponse.parse(reply);
+        if (rsp.isSuccess())
+            return rsp.getNodes();
+        else
+            switch (rsp.getNodeCount()) {
+                case (9998): {
+                    this.unregister(name, me);
+                    return this.register(name, me);
+                }
+                default:
+                    break;
+            }
+        // TODO: Handle other request errors
+        throw new RequestFailedException("Unhandled request error!");
     }
 
-    public boolean unregister(Node me) {
-        throw new NotImplementedException();
+    /**
+     * Unregisters the node bootstrap from server
+     *
+     * @param name name of the client
+     * @param me   node represented by the name
+     * @return whether the response is success
+     */
+    public boolean unregister(String name, Node me) {
+        UnregisterRequest msg = new UnregisterRequest(name, me);
+        LOGGER.info(String.format("Requesting Bootstrap Server: %s", msg.toString()));
+        String reply = request(msg.toString()); // Method will wait for reply
+        LOGGER.info(String.format("Bootstrap Server replied: %s", reply));
+        UnregisterResponse rsp = UnregisterResponse.parse(reply);
+        super.close();
+        return rsp.isSuccess();
     }
 }
