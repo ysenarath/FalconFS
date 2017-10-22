5b7a700faa6df0edfc4d238a9f0d3a3b877b5cd2
Nadheesh <nadheeshj@gmail.com>
2017-10-22 20:53:15 +0530

    Implementing the node selector algorithm

--- src/main/java/lk/uomcse/fs/model/NodeSelector.java
+++ src/main/java/lk/uomcse/fs/model/NodeSelector.java
@@ -2,12 +2,52 @@ package lk.uomcse.fs.model;
 
 import lk.uomcse.fs.entity.Node;
 
-import java.util.Set;
+import java.util.*;
 
 /**
  * Select the best nodes to request the file
  */
 public class NodeSelector {
 
-//    public NodeSelector(Set<Node>,)
+    private static final int  MAX_NODES = 5;
+
+    private static final int MIN_REQ_HEALTH = 50;
+
+    private final Set<Node> neighbours;
+
+    private final CacheService cacheService;
+
+    public NodeSelector(Set<Node> neighbours,CacheService cacheService){
+        this.neighbours = neighbours;
+        this.cacheService = cacheService;
+    }
+
+    /**
+     * Select best nodes from the cached nodes and the neighbours
+     * @param fileName
+     * @return
+     */
+    public List<Node> selectBestNodes(String fileName){
+
+        List<Node> bestNodes = cacheService.search(fileName);
+
+        int nodeGap = MAX_NODES - bestNodes.size();
+        int fromNeighbours =  nodeGap > 0 ? nodeGap + 2 : 2;
+
+        List<Node> neighbourList;
+        synchronized(neighbours) {
+            neighbourList = new ArrayList<>(neighbours);
+        }
+
+        Collections.sort(neighbourList);
+
+        Iterator<Node> neighbourIterator = neighbourList.iterator();
+        while (neighbourIterator.hasNext() && fromNeighbours > 0){
+            bestNodes.add(neighbourIterator.next());
+            fromNeighbours--;
+        }
+
+        return bestNodes;
+    }
+
 }
