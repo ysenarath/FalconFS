5a507800d84b2d7d75f95bcc283a41cbf10ca6c9
Nadheesh <nadheeshj@gmail.com>
2017-10-25 15:05:50 +0530

    Changing response file name format to support msg format

--- src/main/java/lk/uomcse/fs/model/QueryService.java
+++ src/main/java/lk/uomcse/fs/model/QueryService.java
@@ -224,7 +224,7 @@ public class QueryService {
                 }
             }
             if (isMatch){
-                found.add(filename);
+                found.add(filename.replace(' ', '_'));
             }
 
         }
