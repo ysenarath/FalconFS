22fa2977c8e3de595a6468964aeba5258ca6db7d
Nadheesh <nadheeshj@gmail.com>
2017-11-04 21:48:30 +0530

    Adding rest receiver and sender classes

---
+++ src/main/java/lk/uomcse/fs/com/RestReceiver.java
@@ -0,0 +1,11 @@
+package lk.uomcse.fs.com;
+
+import lk.uomcse.fs.entity.Message;
+
+//TODO - Implement
+public class RestReceiver extends Receiver {
+    @Override
+    public Message receive() throws InterruptedException {
+        return null;
+    }
+}
---
+++ src/main/java/lk/uomcse/fs/com/RestSender.java
@@ -0,0 +1,12 @@
+package lk.uomcse.fs.com;
+
+import lk.uomcse.fs.entity.Message;
+
+//TODO - Implement
+public class RestSender extends Sender {
+
+    @Override
+    public void send(Message packet) {
+
+    }
+}
