fbf247130081bd7cb6efcf07a0924a088993f008
Nadheesh <nadheeshj@gmail.com>
2017-11-07 20:35:13 +0530

    Implementing rest receiver and sender

--- src/main/java/lk/uomcse/fs/com/RestReceiver.java
+++ src/main/java/lk/uomcse/fs/com/RestReceiver.java
@@ -1,11 +1,59 @@
 package lk.uomcse.fs.com;
 
-import lk.uomcse.fs.messages.IMessage;
+import lk.uomcse.fs.messages.JoinRequest;
+import lk.uomcse.fs.messages.JoinResponse;
+import lk.uomcse.fs.messages.SearchRequest;
+import lk.uomcse.fs.messages.SearchResponse;
 
-//TODO - Implement
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+@Path("/")
 public class RestReceiver extends Receiver {
-    @Override
-    public IMessage receive() throws InterruptedException {
-        return null;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        return "Receiver activated!";
+    }
+
+    @POST
+    @Path(JoinRequest.ID)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response joinRequest(JoinRequest joinRequest, @Context HttpServletRequest req) {
+        System.out.println(req.getRemoteHost());
+        System.out.println(req.getRemoteAddr());
+        System.out.println(req.getRemotePort());
+        System.out.println(joinRequest.getID());
+        messages.offer(joinRequest);
+        return Response.status(200).entity("join request received").build();
+    }
+
+    @POST
+    @Path(JoinResponse.ID)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response joinResponse(JoinResponse joinResponse) {
+        messages.offer(joinResponse);
+        return Response.status(200).entity("join response received").build();
+    }
+
+    @POST
+    @Path(SearchRequest.ID)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response searchRequest(SearchRequest searchRequest) {
+        messages.offer(searchRequest);
+        return Response.status(200).entity("search request received").build();
+    }
+
+    @POST
+    @Path(SearchResponse.ID)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response searchResponse(SearchRequest searchRequest) {
+        messages.offer(searchRequest);
+        return Response.status(200).entity("search response received").build();
     }
 }
--- src/main/java/lk/uomcse/fs/com/RestSender.java
+++ src/main/java/lk/uomcse/fs/com/RestSender.java
@@ -1,11 +1,28 @@
 package lk.uomcse.fs.com;
 
 import lk.uomcse.fs.messages.IMessage;
+import lk.uomcse.fs.messages.JoinRequest;
 
-//TODO - Implement
+import javax.ws.rs.client.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 public class RestSender extends Sender {
+
+    private Client client = ClientBuilder.newClient();
+
     @Override
     public void send(String ip, int port, IMessage request) {
+
+        WebTarget webTarget = client
+                .target(String.format("%s:%d", ip, port))
+                .path(String.format("/%s", request.getID()));
+
+        Invocation.Builder builder = webTarget.request(MediaType.APPLICATION_JSON);
+        Response response = builder.post(Entity.entity(request, MediaType.APPLICATION_JSON));
+        if (response.getStatus() != 200) {
+            throw new RuntimeException(String.format("The request sending failed with status %s.", response.getStatusInfo()));
+        }
 
     }
 }
