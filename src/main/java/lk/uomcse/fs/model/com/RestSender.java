b00d07dc1219b52cfdf8549b48325876fa5ea71d
Yasas <wayasas@gmail.com>
2017-11-09 15:45:54 +0530

    changing timeout to 800 ms

--- src/main/java/lk/uomcse/fs/model/com/RestSender.java
+++ src/main/java/lk/uomcse/fs/model/com/RestSender.java
@@ -24,8 +24,8 @@ public class RestSender extends Sender {
     public RestSender(Node self) {
         this.self = self;
         ClientConfig configuration = new ClientConfig();
-        configuration.property(ClientProperties.CONNECT_TIMEOUT, 1000);
-        configuration.property(ClientProperties.READ_TIMEOUT, 1000);
+        configuration.property(ClientProperties.CONNECT_TIMEOUT, 800);
+        configuration.property(ClientProperties.READ_TIMEOUT, 800);
 
         this.client = ClientBuilder.newClient(configuration);
 
